26ad81868f80cd03a79eca5eaac14359c71131b5
Chandra-Kurnia <chandrakqurniawan2002@gmail.com>
2021-10-24 06:41:08 +0700

    nesting pseudo elemen

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,33 +1,39 @@
 <template>
-    <div class="NavbarWrapper">
-        <span>Hello Candra</span>
-        <slot></slot>
-    </div>
+  <div class="NavbarWrapper">
+    <span>Hello Candra</span>
+    <slot></slot>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Navbar'
-}
+  name: 'Navbar',
+};
 </script>
 
 <style lang="scss" scoped>
-.NavbarWrapper{
-    width: 100%;
-    height: 70px;
-    background-color: $vue-color;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
+.NavbarWrapper {
+  width: 100%;
+  height: 70px;
+  background-color: $vue-color;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 
-span{
+  &:hover{
+      background-color: $primary-color;
+      span{
+          color: blue;
+      }
+  }
+
+  span {
     display: block;
     font-size: 30px;
-    
+
     /* example using  map variable*/
     color: map-get($alert, 'primary');
+  }
 }
-}
-
 </style>
