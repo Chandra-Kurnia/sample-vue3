03f00b23d46b37873d77a4761ae0e82d7ad473d7
Chandra-Kurnia <chandrakqurniawan2002@gmail.com>
2021-10-24 07:17:48 +0700

    example mixin with arguments

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,44 @@
 <template>
-    <h1>Ini halaman home</h1>
+  <h1>Ini halaman home</h1>
+  <div class="list">
+    <ul>
+      <li>List 1</li>
+      <li>List 2</li>
+      <li>List 3</li>
+      <li>List 4</li>
+      <li>List 5</li>
+    </ul>
+  </div>
+  <div class="list2">
+    <ul>
+      <li>List 1</li>
+      <li>List 2</li>
+      <li>List 3</li>
+      <li>List 4</li>
+      <li>List 5</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Home'
-}
+  name: 'Home',
+};
 </script>
+
+<style lang="scss" scoped>
+.list{
+    /* call mixin */
+    @include flex();
+    ul{
+        @include list(salmon, 20px, 20px);
+    }
+}
+
+.list2{
+    @include flex();
+    ul{
+        @include list(gray, 30px, 50px);
+    }
+}
+</style>
